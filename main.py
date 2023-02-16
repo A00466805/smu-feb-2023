e484965b5f758ca6178d51d07021fd13aea8a18f
Shreerag Marar <shreerag.krishnakumar.marar@smu.ca>
2023-02-15 20:02:46 -0400

    added joblib

--- main.py
+++ main.py
@@ -7,5 +7,16 @@ Created on Wed Feb 15 19:39:11 2023
 """
 
 import streamlit as st
+from joblib import load
+
 
 st.title("Deploying the model")
+clf = load("DT.joblib")
+LABELS = ['setosa', 'versicolor', 'virginica']
+sp_l = st.slider('sepal length (cm)', min_value=0,max_value = 10)
+sp_w = st.slider('sepal width (cm)',min_value=0,max_value=10)
+pe_l = st.slider('petal length (cm)',min_value=0,max_value=10)
+pe_w = st.slider('petal width (cm)',min_value=0,max_value=10)
+
+prediction = clf.predict([[sp_l,sp_w,pe_l,pe_w]])
+st.write(LABELS[prediction[0]])
